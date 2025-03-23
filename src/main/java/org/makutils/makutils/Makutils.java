cf84ffa8cce3ecb7e754f1e3d48e2e853c3184aa
makis <makislaskaris@gmail.com>
2025-03-23 16:54:27 +0200

    Added custom join message using the deprecated method, if the other one breaks revert to this one pls.

---
+++ src/main/java/org/makutils/makutils/Makutils.java
@@ -0,0 +1,39 @@
+package org.makutils.makutils;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.awt.*;
+
+public final class Makutils extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Makutils has been summoned!! :)");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    // Change Join Message
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getName();
+        String joinMassage = "{player} has arrived! Everybody act natural!";
+        String message = joinMassage.replace("{player}", playerName);
+        event.setJoinMessage(message);
+    }
+
+
+
+
+    // Boilerplate, don't touch this except if you know what you are doing pls.
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("Makutils shut down, :(((");
+    }
+}
